3daeb5e64927193fa9336e161b9217b0902c7748
benten63-appdev4ever <138107061+benten63-appdev4ever@users.noreply.github.com>
2023-06-29 16:21:42 +0530

    Update streamlight_app.py
    added pandas

--- streamlight_app.py
+++ streamlight_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('Hello to Streamlit')
 streamlit.header('Breakfast items')
 streamlit.text('🥣 Omega')
@@ -7,4 +8,7 @@ streamlit.text('🐔 egg')
 streamlit.text('🥑🍞 Avacado & toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
  
